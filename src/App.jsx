3e5b5dbc420d230de667487be59dbf23c913d943
Sabi <sabi456@gmail.com>
2023-11-04 20:45:07 -0600

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,31 @@ import { Button } from "./components/Button";
 
 function App() {
   const [calc, setCalc] = useState("");
-  const [result, setResult] = useState(""); //null = valor inicial
+  const [result, setResult] = useState(""); //valor inicial
+
   const dataNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const dataOperator = ["+", "-", "/", "*", "."];
 
+
   const handleClick = value => {
     if (
-      dataOperator.includes(value) && calc === "" ||
-      dataOperator.includes(value) && dataOperator.includes(calc.slice(-1)) //like this it's not possible to add more than operator seguidos
+      dataOperator.includes(value) && calc === "" || // if the last value is an operator and the calc is ""
+      dataOperator.includes(value) && dataOperator.includes(calc.slice(-1)) //the current value is an operator and the last value was also an operator: like this it's not possible to add more than one operator seguidos. slice: check if the last value is an operator 
     ) {
       return
     }
     setCalc(calc + value);
 
-    if (!dataOperator.includes(value)) {
-      setResult(eval(calc + value).toString());
+    if (!dataOperator.includes(value)) { // if the last value was not an operator
+      setResult(eval(calc + value).toString()); 
     }
   }
+
 
   const calculate = () => {
     setCalc(eval(calc).toString());
   }
+
 
   const deleteLast = () => {
     if (calc == "") {
@@ -35,22 +39,38 @@ function App() {
     setCalc(value);
   }
     
+  
   return (
     <div>
-      <h1>Calculadora</h1>
+      <h1>Calculadora</h1><br />
 
-      <div className="display">
+    <div className="bg-cyan-900 p-3.5 rounded-lg">
+
+      <div className="text-black h-12 text-right w-full bg-slate-300 rounded-lg p-2 text-left mt-5 mb-4">
         {result ? <span>({result})</span> : ""} {calc || "0"}
       </div>
 
+      <div className="grid grid-cols-4 pt-4 gap-3">
 
-      {dataNum.map((num, i) => <Button key={i} t={num} handleClick={handleClick} />)}
+        <div className="grid grid-cols-3 grid-rows-4 justify-items-center gap-2 col-span-3 ">
 
-      {dataOperator.map((op, i) => <Button key={i} t={op} handleClick={handleClick}/>)}
+          {dataNum.map((num, i) => <Button key={i} t={num} handleClick={handleClick} />)}
 
-      <Button t={"C"} handleClick={deleteLast} />
-      <Button t={"0"} handleClick={handleClick} />
-      <Button t={"="} handleClick={calculate} />
+          <Button t={"C"} handleClick={deleteLast} />
+          <Button t={"0"} handleClick={handleClick} />
+          <Button t={"="} handleClick={calculate} />
+
+        </div>
+
+      <div className="flex flex-col col-span-1 gap-2">
+
+        {dataOperator.map((op, i) => <Button key={i} t={op} handleClick={handleClick}/>)}
+
+      </div>
+
+    </div>
+
+    </div>
 
     </div>
   );
