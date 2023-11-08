0706093771fe85273f3416436dedb2b9cd2ab266
alexander-lh <lasherasalexander@gmail.com>
2023-11-08 18:18:03 +0100

    ft: new update

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -39,25 +39,12 @@ export function Calculator() {
     setOperation("");
   }
 
-  // const handleBackspace = () => {
-  //   setOperation((prevOperation) => {
-  //     const operationParts = prevOperation.split(" ");
-  //     const operationPartsTrim = operationParts.map(space => space.trim());
-  //     if (operationPartsTrim.length > 1 && !operators.includes(operationPartsTrim[operationPartsTrim.length - 1])) {
-  //       operationPartsTrim.pop();
-  //       return operationPartsTrim.join(" ");
-  //     } else {
-  //       return prevOperation.slice(0, -1);
-  //     }
-  //   });
-  // };
 
   return (
     <div>
       <h1>Calculadora</h1>
       <Display result={result} operation={operation} className="h-10 w-full bg-slate-300 rounded-lg p-2 text-left mt-5"/>
       <div className="flex flex-row p-3 gap-3 justify-items-center">
-        {/* <Button label={"C"} hc={handleBackspace} /> */}
       </div>
       <div className="flex flex-row p-3 gap-3 justify-items-center">
         <div className="grid grid-cols-3 gap-3 justify-items-center">
